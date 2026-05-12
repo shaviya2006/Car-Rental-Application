a0897c81709c7d77c3c4ff80bd98cc39e53f9a86
shaviya2006 <wickramarathneshaveen@gmail.com>
2026-05-13 01:36:48 +0530

    implement Booking entity to handle rental transactions

---
+++ src/main/java/com/carrental/car_rental_backend/entity/Booking.java
@@ -0,0 +1,53 @@
+package com.carrental.car_rental_backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
+    private Car car;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status = BookingStatus.PENDING;
+
+    private BigDecimal totalAmount;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public enum BookingStatus {
+        PENDING,
+        APPROVED,
+        REJECTED,
+        COMPLETED,
+        CANCELLED
+    }
+}
