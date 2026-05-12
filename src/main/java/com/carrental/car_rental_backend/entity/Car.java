b510b5925f0ca34a022500d4438c3da40086e137
shaviya2006 <wickramarathneshaveen@gmail.com>
2026-05-13 01:33:45 +0530

    implementing car entity

---
+++ src/main/java/com/carrental/car_rental_backend/entity/Car.java
@@ -0,0 +1,49 @@
+package com.carrental.car_rental_backend.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cars")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private Integer year;
+
+    @Column(nullable = false)
+    private BigDecimal dailyPrice;
+
+    private String fuelType;
+
+    private String type;
+
+    @Enumerated(EnumType.STRING)
+    private CarStatus status = CarStatus.AVAILABLE;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public enum CarStatus {
+        AVAILABLE,
+        RENTED,
+        MAINTENANCE
+    }
+}
---
+++ src/main/java/com/carrental/car_rental_backend/repository/CarRepository.java
@@ -0,0 +1,20 @@
+package com.carrental.car_rental_backend.repository;
+
+
+import com.carrental.car_rental_backend.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Integer> {
+
+
+    List<Car> findByStatus(Car.CarStatus status);
+
+
+    List<Car> findByBrandIgnoreCase(String brand);
+
+
+    List<Car> findByStatusOrderByDailyPriceAsc(Car.CarStatus status);
+}
