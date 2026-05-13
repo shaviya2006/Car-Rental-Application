0a55d5063f6bedbeed2c65b3688c626ea1dfe4fa
shaviya2006 <wickramarathneshaveen@gmail.com>
2026-05-13 23:52:18 +0530

    Fixing the syntax of the CarRepository

--- src/main/java/com/carrental/car_rental_backend/repository/CarRepository.java
+++ src/main/java/com/carrental/car_rental_backend/repository/CarRepository.java
@@ -4,7 +4,7 @@ package com.carrental.car_rental_backend.repository;
 import com.carrental.car_rental_backend.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.
+import java.util.*;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
